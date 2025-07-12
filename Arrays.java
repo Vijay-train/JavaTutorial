65adb7a57cde12b2244348a0480b255cb87afb12
Vijay-train <53718047+Vijay-train@users.noreply.github.com>
2025-07-12 16:22:39 +0000

    more functions

--- Arrays.java
+++ Arrays.java
@@ -12,13 +12,17 @@ import java.lang.System;
 
 8) Reverse the order of elements in an array.
 9) Determine if an array of numbers is sorted in ascending order.
-10)Given an array with duplicate elements, create a new array with only unique elements.
-11)Write a program to rotate an array to the left by one position.
 12)Find the second largest element in an array.
-13)Given an array of integers and a target sum, find if there exists a pair of elements in the array that add up to the target sum.
+
+13)Given an array of integers and a target sum, 
+find if there exists a pair of elements in the array that add up to the target sum.
 14)Given an array containing n distinct numbers taken from 0, 1, 2, ..., n, find the one that is missing.
-15)Count the frequency of each element in an array.
-16)Given an array of integers, count the number of even and odd numbers in it.
+
+Homework
+* 10)Given an array with duplicate elements, create a new array with only unique elements.
+* 11)Write a program to rotate an array to the left by one position.
+* 15)Count the frequency of each element in an array.
+* 16)Given an array of integers, count the number of even and odd numbers in it.
 
 */
 public class Arrays {
@@ -111,20 +115,111 @@ public class Arrays {
    public static void ReverseArray(int[] arr){
       // create a new array, and store the value in reverse order of arr
       // print the reversed array
+      /*
+       * 10 .. 10
+       * Arr[0]->10, Arr[1] 20 .. 20,10
+       * temp = Arr[0] // 10   
+       * Arr[0] = Arr[1] // 20
+       * Arr[1] = temp
+       * 
+       * 10,20,30,40 --> 40,30,20,10
+       * 
+       * inplace reversal
+       * 
+       */
+
+       //   int[] testArr = {10,20,30,40,50};
+       int[] reversedArray = new int[arr.length];
+
+       for (int i = 0; i < arr.length; i++){
+         reversedArray[i] = arr[arr.length-1-i]; //arr[4]
+       }
+
+       System.out.println("Printing the Reversed Array");
+       for (int i = 0 ; i < reversedArray.length; i++){
+         System.out.println((reversedArray[i]));
+       }
    }
 
-     public static void main(String[] args) {
-       int[] testArr = {10,20,30,40,50};
-   
+   //9) Determine if an array of numbers is sorted in ascending order.
+   // 1,4,5,6,7,8 .."Array is in sorted ascending order"
+   // 1,4,3,5,6 .."Arrays is NOT in sorted ascending order"
+   public static void IsSorted(int[] arr){
+ 
+      boolean isSorted = true;
+       for (int i = 0; i < arr.length - 1; i++){
+         if (arr[i] > arr[i+1]){
+            isSorted = false;
+            break;
+         }
+       }
 
+       if (isSorted == true){
+         System.out.println("Array is in sorted ascending order");
+       }
+       else{
+         System.out.println("Array is NOT in sorted ascending order");
+       }
+
+
+   }
+
+   // 12)Find the second largest element in an array.
+   // for eg - {10, 5, 20, 8, 25, 15}; , this should print 20
+   public static void PrintSecondLargest(int[] arr){
+      int largest = 0;
+      int secondLargest = 0;
+
+      for (int i = 0 ; i < arr.length ; i++){
+         if (arr[i] > largest ){// this means current element is the largest
+            secondLargest = largest;
+            largest = arr[i];
+         } 
+         else if (arr[i] > secondLargest && arr[i] != largest){
+            secondLargest = arr[i];
+         }
+      }
+
+      System.out.println("The second largest element in the array is " + secondLargest);
+   }
+   
+// 13)Given an array of integers and a target sum, 
+// find if there exists a pair of elements in the array that add up to the target sum.
+
+// {10,5,25,8,25,15} , targetSum = 20 .. Yes or no.. Yes = if there is pair that sums to the target sum
+// pair = any pair of 2 elements in the array
+
+   public static void FindPairWithSum(int[] arr, int targetSum){
+      int isPair = 0;
+      for(int i = 0; i < arr.length - 1; i++){
+         if(arr[i] + arr[i+1] == targetSum){
+               System.out.println("Yes! Pair exists");
+               isPair = 1;
+               break;
+         }
+      }
+      if(isPair == 0){
+         System.out.println("NO Pair");
+      }
+    }
+
+
+     public static void main(String[] args) {
+       int[] testArr = {10,5,25,8,25,15};
+   
+       /* 
        PrintArray(testArr);
        PrintSum(testArr);
        PrintLargest(testArr);
-
        FindNumber(15,testArr);
-
        CountOccurence(130,testArr);
+       ReverseArray(testArr);
+       IsSorted(testArr);
+      PrintSecondLargest(testArr);
+       */
 
+      
+      FindPairWithSum(testArr,133);
        
 
      }
