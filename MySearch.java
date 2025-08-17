ec012f2afe0a55ce1585bf6f07951ef054e54100
Vijay-train <53718047+Vijay-train@users.noreply.github.com>
2025-08-17 02:48:26 +0000

    updates

--- MySearch.java
+++ MySearch.java
@@ -1,4 +1,40 @@
+/*
+    Find a number in an unsorted array.
+
+    Count the occurrences of a number in an unsorted array.
+ 
+ *  Question: Write a Java program to perform a binary search on a sorted array of double values.
+      The program should return the index of the element if found, or -1 if not found.
+      Example Input: [1.1, 2.2, 3.3, 4.4, 5.5] and key = 3.3
+      Expected Output: 2
+
+    Find a number in a sorted array
+    Search for a number in a 2D sorted matrix
+    Find the first and last occurrence of a number in a sorted array.
+
+    Find the missing number in a sorted array of distinct numbers from 1 to N.
+    Find a triplet in a sorted array that sums to a given value.
+    Search a key in a sorted infinite array.
+    Find the floor of an element in a sorted array.
+
+    Find the ceiling of an element in a sorted array.
+    Find a Fixed Point (Value equal to index) in a given array.
+    Find the median of two sorted arrays of different sizes.
+— HW—
+*Find the peak element in an array.
+*Find the number of times a sorted array is rotated.
+*Search for a number in an array where adjacent elements differ by at most 1.
+*Find a pair in a rotated sorted array with a given sum.
+*Find the position of an element in a sorted array of infinite numbers.
+*Find the first repeating element in an array of integers.
+*Find the repeating and the missing number.
+*Search in a sorted array of unknown size.
+ */
+
+
+
 public class MySearch {
+
 
     public static void linearSearch(int[] arr, int key) {
         boolean found = false;
